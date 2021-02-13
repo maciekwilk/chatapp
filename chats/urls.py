059a0bcf3432ab29a37ac64bad45e92e6a124db5
Maciek Wilk <maciek.wilk@cbsinteractive.com>
2021-02-13 18:11:42 +0000

    Add message endpoint

--- chats/urls.py
+++ chats/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('messages/', views.MessageListCreate.as_view()),
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 
+from .models import Message
+from .serializers import MessageSerializer
+from rest_framework import generics
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+class MessageListCreate(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
