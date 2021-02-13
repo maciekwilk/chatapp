085596ec3df08221f92aa5dd5a00db1dc2c8f12d
Maciek Wilk <maciek.wilk@cbsinteractive.com>
2021-02-13 22:50:39 +0000

    Add simple interface

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-class App extends Component {
+class Messages extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -33,15 +33,79 @@ class App extends Component {
 
   render() {
     return (
-      <ul>
+      <div>
         {this.state.data.map(message => {
           return (
-            <li key={message.id}>
-              {message.username} - {message.text}
-            </li>
+            <div key={message.id}>
+              {message.username}({message.timestamp}): {message.text}
+            </div>
           );
         })}
-      </ul>
+      </div>
+    );
+  }
+}
+
+class Textfield extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      new_message: 'Type a message'
+    };
+  }
+
+  resetNewMessage() {
+    this.setState({new_message: 'Type a message'});
+  }
+
+  updateNewMessage(message) {
+    this.setState({new_message: message});
+  }
+
+  sendMessage() {
+    const request = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: 'maciek',
+        timestamp: Date.now(),
+        text: this.state.new_message
+      })
+    }
+    fetch('chats/messages/', request);
+    this.resetNewMessage();
+  }
+
+  render() {
+
+    return (
+        <div>
+          <input
+              className="newMessageText"
+              value={this.state.new_message}
+              onChange={e => this.updateNewMessage(e.target.value)}/>
+          <button
+              className="sendMessageButton"
+              onClick={() => this.sendMessage()}
+          >
+            Send
+          </button>
+        </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="chatapp">
+        <div className="messages">
+          <Messages />
+        </div>
+        <div className="textfield">
+          <Textfield />
+        </div>
+      </div>
     );
   }
 }
