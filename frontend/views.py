9d576d4341d65ab9f0d7e27e5fcfe1fd52c38769
Maciek Wilk <maciek.wilk@cbsinteractive.com>
2021-02-22 00:26:06 +0000

    Pass chat room id to React

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'frontend/index.html')
+def index(request, **kwargs):
+    chat = ''
+    if 'chat' in kwargs:
+        chat = kwargs['chat']
+    print(chat)
+    return render(request, 'frontend/index.html', {'chat': chat})
